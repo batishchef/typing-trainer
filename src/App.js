abbc831506a73cc119b26df28b03c5176ca1a6f1
Никита Батищев <nikitaq57@gmail.com>
2024-03-26 00:47:21 +0300

    Revert "Added new page AddText"
    This reverts commit 2d745627cd87f5e3061f00f26b2282b78662304a.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import styles from './App.module.css';
-import AddText from './pages/addText/AddText';
 import Main from './pages/main/Main';
 
 function App() {
   return (
     <div className={styles.App}>
-      {/* <Main /> */}
-      <AddText />
+      <Main />
     </div>
   );
 }
